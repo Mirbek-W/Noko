413394f7e9abe1c0caf6184e821553b57738fe28
Mirbek <81816961+Mirbek-W@users.noreply.github.com>
2022-06-02 15:39:07 +0300

    sdfg

--- js/js.js
+++ js/js.js
@@ -1,19 +1,38 @@
 
 
-function bag(){
-    document.getElementsByClassName('glavn01')[0].style.backgroundColor="rgb(0, 0, 0)";
-    document.getElementsByClassName('card-fix1-color')[0].style.backgroundColor="rgb(0, 0, 0)";
+function bag(){ // черный 
+    // просто главный фон
+    document.getElementsByTagName('body')[0].style.backgroundColor="var(--f2)"; 
+    document.getElementsByClassName('card-fix1-color')[0].style.backgroundColor="var(--f2)";
+    document.getElementsByClassName('glavn01')[0].style.backgroundColor="var(--f2)";
+    // дальше контент
+
+    
+    document.getElementsByClassName('container-fluid')[0].style.backgroundColor="var(--f2con)";
+    
+    document.getElementsByClassName('blok-fix1-menu')[0].style.backgroundColor="var(--f2con)";
+    document.getElementsByClassName('card-fix1-1')[0].style.backgroundColor="var(--f2con)";
+    document.getElementsByClassName('card-bottom-fix')[0].style.backgroundColor="var(--f2con)";
+   
 }
 
 
-function bag1(){
+
+function bag1(){ // белый
+     // просто главный фон 
     document.getElementsByClassName('glavn01')[0].style.backgroundColor="#F0EDF3";
+
+    // дальше контент
     document.getElementsByClassName('card-fix1-color')[0].style.backgroundColor="#F0EDF3";
 }
 
 
-function bag2(){
+function bag2(){ // странный
+
+     // просто главный фон 
     document.getElementsByClassName('glavn01')[0].style.backgroundColor="#1E1932";
+
+     // дальше контент
     document.getElementsByClassName('card-fix1-color')[0].style.backgroundColor="#1E1932";
 }
 
