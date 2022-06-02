a7a4887cb5de27ddb7dbcdad43c6fd62cce84463
Mirbek <81816961+Mirbek-W@users.noreply.github.com>
2022-06-02 13:40:39 +0300

    sefsf

---
+++ js/js.js
@@ -0,0 +1,11 @@
+
+
+function bag(){
+    document.getElementsByClassName('glavn01')[0].style.backgroundColor="rgb(0, 0, 0)";
+}
+
+
+function bag1(){
+    document.getElementsByClassName('glavn01')[0].style.backgroundColor="rgb(255,255,255)";
+}
+
