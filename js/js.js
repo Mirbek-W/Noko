21a9ab780dc143efd67c145e99514d8a1dca1310
Mirbek <81816961+Mirbek-W@users.noreply.github.com>
2022-06-02 14:08:58 +0300

    iuiu

--- js/js.js
+++ js/js.js
@@ -2,10 +2,20 @@
 
 function bag(){
     document.getElementsByClassName('glavn01')[0].style.backgroundColor="rgb(0, 0, 0)";
+    document.getElementsByClassName('card-fix1-color')[0].style.backgroundColor="rgb(0, 0, 0)";
 }
 
 
 function bag1(){
-    document.getElementsByClassName('glavn01')[0].style.backgroundColor="rgb(255,255,255)";
+    document.getElementsByClassName('glavn01')[0].style.backgroundColor="#F0EDF3";
+    document.getElementsByClassName('card-fix1-color')[0].style.backgroundColor="#F0EDF3";
 }
+
+
+function bag2(){
+    document.getElementsByClassName('glavn01')[0].style.backgroundColor="#1E1932";
+    document.getElementsByClassName('card-fix1-color')[0].style.backgroundColor="#1E1932";
+}
+
+
 
