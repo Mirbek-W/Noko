0245cad59efa0b329b37a94d616d508d8e01c59e
Mirbek <81816961+Mirbek-W@users.noreply.github.com>
2022-06-02 20:59:13 +0300

    sdfdg

--- js/js.js
+++ js/js.js
@@ -1,33 +1,15 @@
+var test = document.querySelector(':root');
+
+
 
 
 function bag(){ // черный 
     // просто главный фон
-    document.getElementsByTagName('body')[0].style.backgroundColor="var(--f2)"; 
-    document.getElementsByClassName('card-fix1-color')[0].style.backgroundColor="var(--f2)";
-    document.getElementsByClassName('glavn01')[0].style.backgroundColor="var(--f2)";
-    
-    document.getElementsByClassName('glavn02')[0].style.backgroundColor="var(--f2)";
-    document.getElementsByClassName('blok-glavn01')[0].style.backgroundColor="var(--f2)";
     
 
+    test.style.setProperty('--f1con', '#000000');
     // дальше контент
-
-    
-    document.getElementsByClassName('container-fluid')[0].style.backgroundColor="var(--f2con)";
-    document.getElementsByClassName('card-fix1-1')[0].style.backgroundColor="var(--f2con)";
-    document.getElementsByClassName('card-bottom-fix')[0].style.backgroundColor="var(--f2con)";
-    
-    //кнопки fix 1
-    document.getElementsByClassName('color-set1')[0].style.backgroundColor="var(--f2con)";
-    document.getElementsByClassName('color-set2')[0].style.backgroundColor="var(--f2con)";
-    document.getElementsByClassName('color-set3')[0].style.backgroundColor="var(--f2con)";
-    document.getElementsByClassName('color-set4')[0].style.backgroundColor="var(--f2con)";
-    document.getElementsByClassName('color-set5')[0].style.backgroundColor="var(--f2con)";
-    document.getElementsByClassName('color-set6')[0].style.backgroundColor="var(--f2con)";
-    document.getElementsByClassName('color-modal')[0].style.backgroundColor="var(--f2con)";
-
-
-    document.getElementsByClassName('fix-down')[0].style.backgroundColor="var(--f2con)";
+    test.style.setProperty('--f1', '#15111F');
    
 }
 
@@ -35,58 +17,21 @@ function bag(){ // черный
 
 function bag1(){ // белый
      // просто главный фон 
-     document.getElementsByTagName('body')[0].style.backgroundColor="var(--f1)"; 
-     document.getElementsByClassName('card-fix1-color')[0].style.backgroundColor="var(--f1)";
-     document.getElementsByClassName('glavn01')[0].style.backgroundColor="var(--f1)";
-     document.getElementsByClassName('glavn02')[0].style.backgroundColor="var(--f1)";
-     document.getElementsByClassName('blok-glavn01')[0].style.backgroundColor="var(--f1)";
+     test.style.setProperty('--f1', '#ffffff');
+
      // дальше контент
- 
-     
-     document.getElementsByClassName('container-fluid')[0].style.backgroundColor="var(--f1con)";
-     document.getElementsByClassName('card-fix1-1')[0].style.backgroundColor="var(--f1con)";
-     document.getElementsByClassName('card-bottom-fix')[0].style.backgroundColor="var(--f1con)";
-     document.getElementsByClassName('color-modal')[0].style.backgroundColor="var(--f1con)";
-     
-     //кнопки fix 1
-     document.getElementsByClassName('color-set1')[0].style.backgroundColor="var(--f1con)";
-     document.getElementsByClassName('color-set2')[0].style.backgroundColor="var(--f1con)";
-     document.getElementsByClassName('color-set3')[0].style.backgroundColor="var(--f1con)";
-     document.getElementsByClassName('color-set4')[0].style.backgroundColor="var(--f1con)";
-     document.getElementsByClassName('color-set5')[0].style.backgroundColor="var(--f1con)";
-     document.getElementsByClassName('color-set6')[0].style.backgroundColor="var(--f1con)";
- 
- 
-     document.getElementsByClassName('fix-down')[0].style.backgroundColor="var(--f1con)";
+     test.style.setProperty('--f1con', '#F0EDF3');
 }
 
 
 function bag2(){ // странный
 
-     // просто главный фон 
-     document.getElementsByTagName('body')[0].style.backgroundColor="var(--f3)"; 
-     document.getElementsByClassName('card-fix1-color')[0].style.backgroundColor="var(--f3)";
-     document.getElementsByClassName('glavn01')[0].style.backgroundColor="var(--f3)";
-     document.getElementsByClassName('glavn02')[0].style.backgroundColor="var(--f3)";
-     document.getElementsByClassName('blok-glavn01')[0].style.backgroundColor="var(--f3)";
-     // дальше контент
- 
-     
-     document.getElementsByClassName('container-fluid')[0].style.backgroundColor="var(--f3con)";
-     document.getElementsByClassName('card-fix1-1')[0].style.backgroundColor="var(--f3con)";
-     document.getElementsByClassName('card-bottom-fix')[0].style.backgroundColor="var(--f3con)";
-     document.getElementsByClassName('color-modal')[0].style.backgroundColor="var(--f3con)";
-     
-     //кнопки fix 1
-     document.getElementsByClassName('color-set1')[0].style.backgroundColor="var(--f3con)";
-     document.getElementsByClassName('color-set2')[0].style.backgroundColor="var(--f3con)";
-     document.getElementsByClassName('color-set3')[0].style.backgroundColor="var(--f3con)";
-     document.getElementsByClassName('color-set4')[0].style.backgroundColor="var(--f3con)";
-     document.getElementsByClassName('color-set5')[0].style.backgroundColor="var(--f3con)";
-     document.getElementsByClassName('color-set6')[0].style.backgroundColor="var(--f3con)";
- 
- 
-     document.getElementsByClassName('fix-down')[0].style.backgroundColor="var(--f3con)";
+          // просто главный фон 
+          test.style.setProperty('--f1', '#15111F');
+
+          // дальше контент
+          test.style.setProperty('--f1con', '#1E1932');
+
 }
 
 
