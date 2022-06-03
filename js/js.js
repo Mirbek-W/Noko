6aab4a35efeb254d425f3e2222ec376fe7240b60
Mirbek <81816961+Mirbek-W@users.noreply.github.com>
2022-06-03 11:34:29 +0300

    sdfvs

--- js/js.js
+++ js/js.js
@@ -10,6 +10,9 @@ function bag(){ // черный
     test.style.setProperty('--f1con', '#000000');
     // дальше контент
     test.style.setProperty('--f1', '#15111F');
+    //тект фоон 
+    test.style.setProperty('--f2', '#ffffff');
+    test.style.setProperty('--f2con-fix-text', '#000000');
    
 }
 
@@ -21,6 +24,8 @@ function bag1(){ // белый
 
      // дальше контент
      test.style.setProperty('--f1con', '#F0EDF3');
+     test.style.setProperty('--f2', '#000000');
+     test.style.setProperty('--f2con-fix-text', '#ffffff');
 }
 
 
@@ -32,7 +37,47 @@ function bag2(){ // странный
           // дальше контент
           test.style.setProperty('--f1con', '#1E1932');
 
+          test.style.setProperty('--f2', '#ffffff');
+          test.style.setProperty('--f2con-fix-text', '#000000');
+
 }
+
+
+
+// функции для изменения цветов на кнопках итд текстах
+
+
+function bgbuttontext1(){
+    test.style.setProperty('--f1-button', '#4F38A6');
+
+    
+}
+
+function bgbuttontext2(){
+    test.style.setProperty('--f1-button', '#E6D153');
+
+    
+}
+
+function bgbuttontext3(){
+    test.style.setProperty('--f1-button', '#E34B5E');
+
+    
+}
+
+function bgbuttontext4(){
+    test.style.setProperty('--f1-button', '#4BE59F');
+
+    
+}
+
+function bgbuttontext5(){
+    test.style.setProperty('--f1-button', '#4AABDF');
+
+    
+}
+
+
 
 
 
